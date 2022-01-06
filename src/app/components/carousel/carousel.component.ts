c60df5de01ee1028e3c74b95a3b32ac92cbd5f55
lnzambelli <lnzambelli@gmail.com>
2022-01-06 13:30:14 -0300

    Added-new-products-and-prices

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -4,7 +4,7 @@ import { Component, NgModule, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   template: `
-    <img src="assets/img/home/Home-Portada.jpg" style="width: 100%;margin-top: 56px">
+    <img src="assets/img/home/home-port.jpeg" style="width: 100%;margin-top: 56px">
   `,
   styles: ['img { max-height: 400px }']
 })
