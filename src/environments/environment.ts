00a541df9166bf53d370a98b45b40e86a6e8babb
Luis Zambelli <69328669+lnzambelli@users.noreply.github.com>
2022-06-10 10:17:41 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD9JnB-JyF6bzWyb6qWEYI7AjZ1CuOSP3o",
-    authDomain: "vircrazylove.firebaseapp.com",
-    projectId: "vircrazylove",
-    storageBucket: "vircrazylove.appspot.com",
-    messagingSenderId: "579547271736",
-    appId: "1:579547271736:web:538d90b53161bd9dcc4ccd"
+  
   }
 };
 
